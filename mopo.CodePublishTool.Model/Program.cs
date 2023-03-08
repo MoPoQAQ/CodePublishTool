3040d0a175e79d2f3e41699000eb52f50e70539c
MoPo <mopocode@gmail.com>
2023-03-08 15:16:05 +0800

    添加项目文件。

---
+++ mopo.CodePublishTool.Model/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mopo.CodePublishTool.Model
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
